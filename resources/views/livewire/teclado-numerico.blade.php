979ae2d4d555c3175f3961a2b002e69b4f7ef515
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-01-07 17:17:24 -0600

    Diseño teclado

--- resources/views/livewire/teclado-numerico.blade.php
+++ resources/views/livewire/teclado-numerico.blade.php
@@ -1,3 +1,67 @@
 <div>
-    <h1>Test teclado</h1>
+    <div class="grid grid-cols-3 gap-4 my-4">
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            1
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            2 ABC
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            3 DEF
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            4 GHI
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            5 JKL
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            6 MNO
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            7 PQRS
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            8 TUV
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            9 WXYZ
+            </button>
+        </div>
+
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            *
+            </button>
+        </div>
+
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            0 +
+            </button>
+        </div>
+        <div class="flex justify-center mx-12 cursor-pointer">
+            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            #</button>
+        </div>
+
+
+    </div>
 </div>
