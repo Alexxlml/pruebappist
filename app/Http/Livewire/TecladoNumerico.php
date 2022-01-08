c95c0461ff09566d0d2059eda847464fd3af8547
Alexx_lml <marcoalexiszacariasrubio@gmail.com>
2022-01-07 23:30:46 -0600

    Algoritmo tecleo T9

--- app/Http/Livewire/TecladoNumerico.php
+++ app/Http/Livewire/TecladoNumerico.php
@@ -6,8 +6,72 @@ use Livewire\Component;
 
 class TecladoNumerico extends Component
 {
+
+    public $texto;
+    public $numero_actual = 0, $numero_anterior = 0, $contador = 0, $repeticion = false;
+    public $temp = 0;
+
+    public function mount()
+    {
+    }
+
     public function render()
     {
         return view('livewire.teclado-numerico');
+    }
+
+    public function clicks($numero)
+    {
+        if ($this->temp != 0) {
+            $this->numero_anterior = $this->temp;
+        }
+
+        $this->numero_actual = $numero;
+
+        if ($this->numero_anterior == 0 && $this->temp == 0 && $this->numero_actual > 0 && $this->contador == 0 && $this->repeticion == false) {
+            $this->casosClicks(1);
+        } elseif ($this->numero_anterior > 0 && $this->temp != $this->numero_actual && $this->numero_actual > 0 && $this->contador > 0 && $this->repeticion == false) {
+            $this->casosClicks(2);
+        } elseif ($this->numero_anterior > 0 && $this->temp != $this->numero_actual && $this->numero_actual > 0 && $this->contador > 0 && $this->repeticion == false) {
+            $this->casosClicks(2);
+        } elseif ($this->numero_anterior > 0 && $this->temp > 0 && $this->numero_actual > 0 && $this->contador > 0 && $this->repeticion == false) {
+            $this->casosClicks(3);
+        } elseif ($this->numero_anterior != $this->numero_actual && $this->temp != $this->numero_actual && $this->repeticion == true) {
+            $this->casosClicks(2);
+        } elseif ($this->numero_anterior > 0 && $this->temp > 0 && $this->numero_actual > 0 && $this->contador > 0 && $this->repeticion == true) {
+            $this->casosClicks(1);
+        }
+
+        $this->temp = $this->numero_actual;
+    }
+
+    public function casosClicks($i)
+    {
+        switch ($i) {
+            case 1:
+                $this->contador++;
+                break;
+            case 2:
+                $this->contador = 0;
+                $this->contador++;
+                $this->repeticion = false;
+                break;
+            case 3:
+                $this->contador++;
+                $this->repeticion = true;
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+    public function resetVariables(){
+     $this->numero_actual = 0;
+     $this->numero_anterior = 0;
+     $this->contador = 0;
+     $this->repeticion = false;
+     $this->temp = 0;
     }
 }
--- resources/views/livewire/teclado-numerico.blade.php
+++ resources/views/livewire/teclado-numerico.blade.php
@@ -1,67 +1,94 @@
 <div>
     <div class="grid grid-cols-3 gap-4 my-4">
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(1)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             1
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(2)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             2 ABC
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(3)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             3 DEF
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(4)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             4 GHI
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(5)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             5 JKL
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(6)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             6 MNO
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(7)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             7 PQRS
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(8)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             8 TUV
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(9)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             9 WXYZ
             </button>
         </div>
 
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(10)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             *
             </button>
         </div>
 
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(11)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             0 +
             </button>
         </div>
         <div class="flex justify-center mx-12 cursor-pointer">
-            <button class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
+            <button wire:click="clicks(12)"class="grow bg-slate-800 text-gray-50 shadow-md rounded px-16 py-3 hover:scale-110 focus:outline-none focus:ring focus:ring-blue-500 focus:bg-blue-900">
             #</button>
         </div>
-
-
     </div>
+
+    <section>
+        <div>
+            <p>
+            numero anterior: 
+            {{ $numero_anterior}}
+            </p>
+            <p>
+            temp: 
+            {{ $temp }}
+            </p>
+            <p>
+            numero actual: 
+            {{ $numero_actual }}
+            </p>
+            <p>
+            contador: 
+            {{ $contador }}
+            </p>
+            <p>
+            repeticion: 
+            @if($repeticion == false)
+                Falso
+            @else
+                Cierto
+            @endif
+            </p>
+        </div>
+    </section>
 </div>
